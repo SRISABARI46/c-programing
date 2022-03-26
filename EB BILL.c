0d8f1440f0964b28b9d98b682d392c586c7c8259
SRISABARI <87270623+SRISABARI46@users.noreply.github.com>
2022-03-26 14:06:48 +0530

    Add files via upload

---
+++ EB BILL.c
@@ -0,0 +1,55 @@
+ 
+#include <stdio.h>
+
+int main()
+ 
+{
+
+ float unit,cost;
+ 
+ while(1)
+ {
+  
+  printf("\nENTER THE USAGE OF UNIT : ");
+
+  scanf("%f",&unit);
+  
+ 	
+if(unit<=0 && unit<=100)
+  
+  	{
+       
+ 	printf("NO COST");
+  
+  	}
+
+	else if (unit>100 && unit<=300)
+    
+ 	{
+        
+	cost = unit*10;
+   
+  	}
+
+	else if (unit>300 && unit<=500)
+   
+  	{
+     
+    	cost = unit*12;
+  	}
+   
+	else if(unit>=500)
+   
+  	{
+     
+    	cost = unit*20;
+   
+   	}
+
+     printf("THE ELECTRICITY BILL FOR THIS MONTH IS : Rs. %0.2f",cost);
+
+ }
+   
+ return 0;
+
+}
